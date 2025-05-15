93e1e9df12dab51e2c3e2f4f7c83788a5293f439
VishalVishwakarma553 <143621029+VishalVishwakarma553@users.noreply.github.com>
2025-05-15 21:03:45 +0530

    Remove loader

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -23,12 +23,12 @@ function App() {
     checkAuth();
   }, [checkAuth]);
 
-  if (isCheckingAuth && !authUser)
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <Loader className="size-10 animate-spin" />
-      </div>
-    );
+  // if (isCheckingAuth && !authUser)
+  //   return (
+  //     <div className="flex items-center justify-center h-screen">
+  //       <Loader className="size-10 animate-spin" />
+  //     </div>
+  //   );
 
   return (
     <div>
